441bbf260b78e479ee04b37177c0994e03c6da00
Daniele Scasciafratte <mte90net@gmail.com>
2019-01-09 13:03:47 +0100

    rewrite tax part, not tested

--- command.php
+++ command.php
@@ -24,8 +24,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 function codeat_get_by_url( $args ){    
     $url = parse_url( esc_html( $args[0] ) );
     $slug = $url[ 'path' ];
-    $last_slug = array_filter( explode( '/', $slug ), 'strlen' );
-    $last_slug = $last_slug[ count( $last_slug ) ];
+    $split_slug = array_filter( explode( '/', $slug ), 'strlen' );
+    $last_slug = $split_slug[ count( $split_slug ) ];
+    $term_slug = $split_slug[ count( $split_slug ) -1 ];
     
     $post = get_posts( array(
             'name' => $last_slug,
@@ -39,12 +40,14 @@ function codeat_get_by_url( $args ){
         }
     }
     
-    $taxonomies = get_taxonomies();
-    foreach ( $taxonomies as $tax_type_key => $taxonomy ) {
-        $tax = get_term_by( 'slug', $last_slug, $taxonomy );
-        if( is_object( $tax ) ) {
-            WP_CLI::log( 'term | ' . $tax->term_id . ' | ' . $tax->taxonomy );
-            return;
+    $taxonomies = get_taxonomies( array(), 'objects' );
+    foreach ( $taxonomies as $taxonomy ) {
+        if ( $taxonomy->rewrite[ 'slug' ] === $term_slug ) {
+            $tax = get_term_by( 'slug', $last_slug, $term_slug );
+            if( is_object( $tax ) ) {
+                WP_CLI::log( 'term | ' . $tax->term_id . ' | ' . $tax->taxonomy );
+                return;
+            }
         }
     }
 };
